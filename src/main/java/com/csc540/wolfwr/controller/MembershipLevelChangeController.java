1bf93bd2b560f735842b48d1b9171c266280f54f
Nik Gudmundsson <91502719+NikGudmundsson@users.noreply.github.com>
2025-04-06 08:49:34 -0400

    Uploading first pass of Controller files.
    There's some issue with my git config and I may have to reset a bunch of credential. Before I do that, I'm just going to ensure that my work is saved to a branch.
    
    Code for the first pass of the Controller files is contained in this checking. Note, Ansh already hit Stores so that was done.

---
+++ src/main/java/com/csc540/wolfwr/controller/MembershipLevelChangeController.java
@@ -0,0 +1,67 @@
+package com.csc540.wolfwr.controller;
+
+import com.csc540.wolfwr.dto.MembershipLevelChangeDTO;
+import com.csc540.wolfwr.service.MembershipLevelChangeService;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Tag(name = "MembershipLevelChange API", description = "CRUD operations for Membership Level Changes")
+@RestController
+@RequestMapping("/membership-level-change")
+public class MembershipLevelChangeController {
+    private final MembershipLevelChangeService membershipLevelChangeService;
+
+    public MembershipLevelChangeController(MembershipLevelChangeService membershipLevelChangeService) {
+        this.membershipLevelChangeService = membershipLevelChangeService;
+    }
+
+    @Operation(summary = "Create a new member level change", 
+               description = "Creates a new membership level change with the provided details")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully created membership level change"),
+            @ApiResponse(responseCode = "400", description = "Invalid input")
+    })
+    @PostMapping
+    public ResponseEntity<MembershipLevelChangeDTO> createMembershipLevelChange(@Valid @RequestBody MembershipLevelChangeDTO membershipLevelChangeDTO) {
+        MembershipLevelChangeDTO createdMembershipLevelChange = membershipLevelChangeService.createMembershipLevelChange(membershipLevelChangeDTO);
+        return ResponseEntity.ok(createdMembershipLevelChange);
+    }
+
+    @Operation(summary = "Get all membership level changes", description = "Retrieves all membership level changes")
+    @ApiResponse(responseCode = "200", description = "Successfully retrieved list")
+    @GetMapping
+    public ResponseEntity<List<MembershipLevelChangeDTO>> getAllMembershipLevelChanges() {
+        List<MembershipLevelChangeDTO> list = membershipLevelChangeService.getAllMembershipLevelChanges();
+        return ResponseEntity.ok(list);
+    }
+
+    @Operation(summary = "Update an existing membership level change", description = "Updates a membership level change")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully updated membership level change"),
+            @ApiResponse(responseCode = "400", description = "Invalid input")
+    })
+    @PutMapping
+    public ResponseEntity<MembershipLevelChangeDTO> updateStaff(@RequestParam Integer memberId, 
+                                                @RequestParam LocalDate date,
+                                                @Valid @RequestBody MembershipLevelChangeDTO membershipLevelChangeDTO) {
+        MembershipLevelChangeDTO updatedMembershipLevelChange = membershipLevelChangeService.updateMembershipLevelChange(membershipLevelChangeDTO, memberId, date);
+        return ResponseEntity.ok(updatedMembershipLevelChange);
+    }
+
+    @Operation(summary = "Delete a member level change", description = "Deletes a member level change based on member and product batch ID")
+    @ApiResponse(responseCode = "204", description = "Successfully deleted member level change")
+    @DeleteMapping
+    public ResponseEntity<Void> deleteMembershipLevelChange(@RequestParam Integer memberId, @RequestParam LocalDate date) {
+        membershipLevelChangeService.deleteMembershipLevelChange(memberId, date);
+        return ResponseEntity.noContent().build();
+    }
+}
---
+++ src/main/java/com/csc540/wolfwr/controller/TransactionItemController.java
@@ -0,0 +1,65 @@
+package com.csc540.wolfwr.controller;
+
+import com.csc540.wolfwr.dto.TransactionItemDTO;
+import com.csc540.wolfwr.service.TransactionItemService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Tag(name = "Transaction Item API", description = "CRUD operations for transaction items")
+@RestController
+@RequestMapping("/transaction-item")
+public class TransactionItemController {
+    private final TransactionItemService transactionItemService;
+
+    public TransactionItemController(TransactionItemService transactionItemService) {
+        this.transactionItemService = transactionItemService;
+    }
+
+    @Operation(summary = "Create a new transaction item", description = "Creates a new transaction item with the provided details")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully created transaction item"),
+            @ApiResponse(responseCode = "400", description = "Invalid input")
+    })
+    @PostMapping
+    public ResponseEntity<TransactionItemDTO> createTransactionItem(@Valid @RequestBody TransactionItemDTO transactionItemDTO) {
+        TransactionItemDTO createdTransactionItem = transactionItemService.createTransactionItem(transactionItemDTO);
+        return ResponseEntity.ok(createdTransactionItem);
+    }
+
+    @Operation(summary = "Get all transaction items", description = "Retrieves all transaction items")
+    @ApiResponse(responseCode = "200", description = "Successfully retrieved list")
+    @GetMapping
+    public ResponseEntity<List<TransactionItemDTO>> getAllTransactionItems() {
+        List<TransactionItemDTO> list = transactionItemService.getAllTransactionItems();
+        return ResponseEntity.ok(list);
+    }
+
+    @Operation(summary = "Update an existing transaction item", description = "Updates a transaction item's details")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully updated transaction item"),
+            @ApiResponse(responseCode = "400", description = "Invalid input")
+    })
+    @PutMapping
+    public ResponseEntity<TransactionItemDTO> updateTransactionItem(@RequestParam Integer transactionId,
+                                                                    @RequestParam Integer productBatchId,
+                                                @Valid @RequestBody TransactionItemDTO transactionItemDTO) {
+        TransactionItemDTO updatedTransactionItem = transactionItemService.updateTransactionItem(transactionItemDTO, transactionId, productBatchId);
+        return ResponseEntity.ok(updatedTransactionItem);
+    }
+
+    @Operation(summary = "Delete a transaction item", description = "Deletes a transaction item by its transaction and product batch ID")
+    @ApiResponse(responseCode = "204", description = "Successfully deleted transaction item")
+    @DeleteMapping
+    public ResponseEntity<Void> deleteTransactionItem(@RequestParam Integer transactionId,
+                                            @RequestParam Integer productBatchId) {
+        transactionItemService.deleteTransactionItem(transactionId, productBatchId);
+        return ResponseEntity.noContent().build();
+    }
+}
