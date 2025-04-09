89c980f4df9f16af68dae69b9486ed4ab8d572cd
Krithika Ragothaman <krithika.ragoth@gmail.com>
2025-04-09 14:39:54 -0400

    basic flow for supplier to create a new shipment from supplier view controller

--- src/main/java/com/csc540/wolfwr/controller/SupplierViewController.java
+++ src/main/java/com/csc540/wolfwr/controller/SupplierViewController.java
@@ -1,6 +1,11 @@
 package com.csc540.wolfwr.controller;
 
+import com.csc540.wolfwr.dto.ShipmentDTO;
+import com.csc540.wolfwr.service.ShipmentService;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +13,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/supplier")
 public class SupplierViewController {
+
+    private final ShipmentService shipmentService;
+
+    public SupplierViewController(ShipmentService shipmentService) {
+        this.shipmentService = shipmentService;
+    }
+
+    @PostMapping("/create-shipment")
+    public ResponseEntity<Integer> createNewShipment(@RequestBody ShipmentDTO shipmentDTO) {
+        ShipmentDTO createdShipment = shipmentService.createShipment(shipmentDTO);
+        return ResponseEntity.ok(createdShipment.getShipmentId());
+    }
 }
